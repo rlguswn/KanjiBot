0b2c78c1f9ed22e93c5a9578a04c07a08d762d65
주기현 <0318joo@naver.com>
2025-05-08 18:40:08 +0900

    Spring Batch 기반 프로젝트 초기 세팅

---
+++ src/main/java/com/example/KanjiBot/KanjiBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.KanjiBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KanjiBot/KanjiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KanjiBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
