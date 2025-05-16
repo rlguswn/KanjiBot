99edf5de2a873d94fc83bbf39bdb03a902eb29c6
주기현 <0318joo@naver.com>
2025-05-16 22:45:42 +0900

    MessageListener를 통해 KanjiChannel 등록 기능 구현

--- src/main/java/com/example/KanjiBot/bot/MessageListener.java
+++ src/main/java/com/example/KanjiBot/bot/MessageListener.java
@@ -1,11 +1,19 @@
 package com.example.KanjiBot.bot;
 
+import com.example.KanjiBot.domain.KanjiChannel;
+import com.example.KanjiBot.service.KanjiChannelService;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageListener extends ListenerAdapter {
+
+    private final KanjiChannelService kanjiChannelService;
+
+    public MessageListener(KanjiChannelService kanjiChannelService) {
+        this.kanjiChannelService = kanjiChannelService;
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -17,6 +25,26 @@ public class MessageListener extends ListenerAdapter {
 
         if (content.equals("!ping")) {
             event.getChannel().sendMessage("Pong!").queue();
+        } else if (content.startsWith("!등록")) {
+            String[] split = content.split(" ");
+            if (split.length < 2) {
+                event.getChannel().sendMessage("`!등록 HH:MM` 형식으로 입력해주세요!").queue();
+                return;
+            }
+
+            String sendTime = split[1];
+            String channelId = event.getChannel().getId();
+            String guildId = event.getGuild().getId();
+
+            KanjiChannel kanjiChannel = new KanjiChannel(
+                    0L,
+                    channelId,
+                    guildId,
+                    sendTime
+            );
+
+            kanjiChannelService.createKanjiChannel(kanjiChannel);
+            event.getChannel().sendMessage("`" + sendTime + "`에 Kanji 알림을 등록했습니다!").queue();
         }
     }
 }
