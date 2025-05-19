23a5e324a734c05d76b53a5056d41791aa516604
주기현 <0318joo@naver.com>
2025-05-20 00:46:55 +0900

    kanji_channel 테이블에 channel_id UNIQUE 제약 조건 추가

--- src/main/resources/db/migration/V1_2__create_kanji_channel_table.sql
+++ src/main/resources/db/migration/V1_2__create_kanji_channel_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE kanji_channel (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    channel_id BIGINT NOT NULL,
+    channel_id BIGINT NOT NULL UNIQUE,
     count_number BIGINT NOT NULL,
     guild_id BIGINT NOT NULL,
     send_time VARCHAR(8),
